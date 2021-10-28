791490a5ace4a467fb880a6561521c23179b1ca4
Syed ZainUlAbideen <shahzain30196@gmail.com>
2021-10-28 08:40:56 -0700

    counter update

--- src/__test__/Counter.test.tsx
+++ src/__test__/Counter.test.tsx
@@ -7,36 +7,32 @@ beforeEach(() => {
 })
 
 describe('testing: <Counter />', () => {
-    it('its shows counter header', () => {
+    it('its shows counter header and initial value', () => {
         const header = screen.getByTestId('counterHeader')
         expect(header).toHaveTextContent('My Counter');
-    });
 
-    it('its shows initial counter value 0', () => {
         expect(screen.getByTestId('counter')).toHaveTextContent('0');
     });
 
-    it('its check add counter btn', () => {
+    it('its check counter btn', () => {
+        //ADD-BTN
         expect(screen.getByTestId('add-btn')).toHaveTextContent('+');
-    });
-
-    it('its check subtract counter btn', () => {
+        //SUB-BTN
         expect(screen.getByTestId('sub-btn')).toHaveTextContent('-');
     });
 
     it('its check initial input value 1', () => {
-        const input = screen.getByTestId('input') as HTMLInputElement
-        expect(input.value).toBe('1');
+        expect(screen.getByRole('textbox')).toHaveValue('1');
     });;
 
     it('its check input change value', () => {
-        const input = screen.getByTestId('input') as HTMLInputElement
+        const input = screen.getByTestId('input') as HTMLInputElement;
         fireEvent.change(input, {
             target: {
                 value: '5'
             }
         });
-        expect(input.value).toBe('5')
+        expect(input.value).toBe('5');
     });
 
     it('its check add button change value', () => {
